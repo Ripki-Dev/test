df46d8ff55d1ef43b68c5ef5986da883e10d66fb
Ripki-Dev <92913502+Ripki-Dev@users.noreply.github.com>
2022-10-05 16:47:59 +0700

    update quiz tgl 4 & 5

---
+++ go.mod
@@ -0,0 +1,3 @@
+module skill-test
+
+go 1.17
--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	//masukkan angka
+	//Quiz Selasa 4 Okt
+	var jawaban string
+	var condition = true
+	role := []string{"penyihir", "guard", "warewolf"}
+	fmt.Println("Pilih peran : Penyihir, Guard & Warewolf")
+	for condition {
+		fmt.Println("Masukkan Nama :")
+		fmt.Scanln(&jawaban)
+		fmt.Println(WareWolfCheck(jawaban, role))
+		condition = false
+	}
+
+	//Quiz Rabu 5 Okt
 	fmt.Println(Factorial(5))
+
 }
 func Factorial(numb int) int {
 	result := 1
@@ -12,4 +27,16 @@ func Factorial(numb int) int {
 		result *= i
 	}
 	return result
+}
+
+func WareWolfCheck(usr string, role []string) string {
+
+	for _, i2 := range role {
+		if i2 == usr {
+			return "Selamat Bermain"
+		} else if usr == "" || usr == " " {
+			return "Nama Harus Diisi"
+		}
+	}
+	return "Pilih Peranmu Untuk Mulai"
 }
